83ef97371d059bf3030d8f1c2d7d7e5d932c8a8e
alexsoft23 <alex.hera.101@gmail.com>
2015-12-06 14:48:19 +0200

    покищо ставимо зображення

--- view/start.php
+++ view/start.php
@@ -1,23 +1,5 @@
-<center><pre>
-
-
-
-
-
-
-Hello world!!!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</pre></center>
+<div class="container text-center">
+<p> </p>
+<img src="./img/872172-porsche-wallpaper.jpg" class="img-thumbnail" alt="PORSCHE" width="800" height="600">
+<p> </p>
+</div>
