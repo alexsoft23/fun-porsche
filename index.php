7a1e6e62169608897ca14058bc2795caa487ed52
alexsoft23 <alex.hera.101@gmail.com>
2015-12-06 11:20:40 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    session_start(); 
+    require"./layouts/header.php";
+    if (isset($_GET['action']) &&  ($_GET['action'] == 'login' || $_GET['action'] == 'save_user') || $_GET['action'] == 'logout' )
+		require_once('./control/'.$_GET['action'].'.php');
+    else {
+    if (isset($_GET['action']) && file_exists('./view/'.$_GET['action'].'.php'))
+				require_once('./view/'.$_GET['action'].'.php');
+			else
+				require_once("./view/start.php");
+			
+			if(isset($_SESSION['auth']) && $_SESSION['auth'] == true)
+				require_once("./view/logout.php");
+			else
+				require_once("./view/login.php");
+    }
+    require_once"./layouts/footer.php";
+?>
