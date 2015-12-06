dada0aa8ebd9a92d8564386335e7b6334d542dc7
alexsoft23 <alex.hera.101@gmail.com>
2015-12-06 15:55:10 +0200

    Форма добавлення новин

---
+++ view/add_news.php
@@ -0,0 +1,27 @@
+<div class="container text-center">
+<p> </p>
+<img src="./img/872172-porsche-wallpaper.jpg" class="img-thumbnail" alt="PORSCHE" width="400" height="300">
+<p> </p>
+</div>
+
+<div id="news" class="container-fluid bg-grey text-center">
+	<h2>Форма додавання новини</h2>
+		<div class="row text-center" >
+			<div class="col-sm-2 text-left">
+				<p>Тут ви можете додати свою новину. Новина буде додана після перевірки адміністратором.</p>
+			</div>
+			<div class="col-sm-6 slideanim text-right">
+				<div class="row">
+					<div class="col-sm-12 form-group">
+						<input class="form-control" id="name" name="name" placeholder="Тема новини" type="text" required>
+					</div>
+				</div>
+				<textarea class="form-control" id="comments" name="comments" placeholder="Введіть вашу новину" rows="5"></textarea><br>
+				<div class="row">
+					<div class="col-sm-12 form-group">
+						<button class="btn btn-default pull-right" type="submit">Надіслати</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
