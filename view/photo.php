a655d3f8ef1815f437a1cd1e883ff00477946eeb
alexsoft23 <alex.hera.101@gmail.com>
2015-12-06 15:26:53 +0200

    корекція

--- view/photo.php
+++ view/photo.php
@@ -1,8 +1,7 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.css" rel="stylesheet">
+<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.js"></script>
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-    <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.js"></script>
 <?php
   function excess($files) {
     $result = array();
@@ -18,7 +17,7 @@
 <p> </p>
 <div class="fotorama" align="center">
     <?php for ($i = 0; $i < count($files); $i++) { ?>
-    <img src="<?=$dir."/".$files[$i]?>" alt="" />
+    <img src="<?=$dir."/".$files[$i]?>" alt="" width="800" height="600">
     <?php } ?>
 </div>
 <p> </p>
