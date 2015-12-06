4eec8280e0e1018af76a92316eddc8502d8fbe5a
alexsoft23 <alex.hera.101@gmail.com>
2015-12-06 14:42:34 +0200

    посилання нікуди неведе

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<div id="footer" class="container text-center">
 		<p>Made by Olexandr Herasymchuk</p>
-		<p><a href="/">Chernivtsi 2015 All rights reserved &copy;</a><p>
+		<p><a href="#">Chernivtsi 2015 All rights reserved &copy;</a><p>
 	</div>
 </footer>
 </body>
