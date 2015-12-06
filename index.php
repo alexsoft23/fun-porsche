2226e5590b5a26b8845637bbfc86938ecab859c4
alexsoft23 <alex.hera.101@gmail.com>
2015-12-06 14:56:09 +0200

    зміна меню при авторизації

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
     session_start(); 
-    require"./layouts/header.php";
+	if(isset($_SESSION['auth']) && $_SESSION['auth'] == true)
+				require_once "./layouts/header_auth.php";
+			else
+				require_once "./layouts/header.php";
+				
     if (isset($_GET['action']) &&  ($_GET['action'] == 'login' || $_GET['action'] == 'save_user') || $_GET['action'] == 'logout' )
 		require_once('./control/'.$_GET['action'].'.php');
     else {
@@ -9,10 +13,10 @@
 			else
 				require_once("./view/start.php");
 			
-			if(isset($_SESSION['auth']) && $_SESSION['auth'] == true)
+			/*if(isset($_SESSION['auth']) && $_SESSION['auth'] == true)
 				require_once("./view/logout.php");
 			else
-				require_once("./view/login.php");
+				require_once("./view/login.php");*/
     }
     require_once"./layouts/footer.php";
 ?>
