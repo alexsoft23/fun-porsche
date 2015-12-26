c7c50c5aab180168bd2070f5f3621456b03d7e98
alexsoft23 <alex.hera.101@gmail.com>
2015-12-26 21:36:41 +0200

    винесення спільного

--- view/photo.php
+++ view/photo.php
@@ -2,22 +2,12 @@
 <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.css" rel="stylesheet">
 <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.3/fotorama.js"></script>
 
-<?php
-  function excess($files) {
-    $result = array();
-    for ($i = 0; $i < count($files); $i++) {
-      if ($files[$i] != "." && $files[$i] != "..") $result[] = $files[$i];
-    }
-    return $result;
-  }
-  $dir = "img";
-  $files = scandir($dir);
-  $files = excess($files);
-?>
+<?php require_once ('./control/img.php'); ?>
+
 <p> </p>
 <div class="fotorama" align="center">
-    <?php for ($i = 0; $i < count($files); $i++) { ?>
-    <img src="<?=$dir."/".$files[$i]?>" alt="" width="800" height="600">
+    <?php for ($i = 0; $i <= $count_files; $i++) { ?>
+    <img src="<?=$dir."/".$files[$i]?>" class="img-thumbnail" alt="PORSCHE" width="800" height="600">
     <?php } ?>
 </div>
 <p> </p>
