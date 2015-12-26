94b12ba407727559f3d08450ea9869780131f9b2
alexsoft23 <alex.hera.101@gmail.com>
2015-12-26 21:46:07 +0200

    публікація новин

--- view/start.php
+++ view/start.php
@@ -1,5 +1,20 @@
 <div class="container text-center">
 <p> </p>
-<img src="./img/872172-porsche-wallpaper.jpg" class="img-thumbnail" alt="PORSCHE" width="800" height="600">
+<?php require_once ('./control/img.php'); ?>
+<?php
+		$fields = [];
+		$fields['published'] = true;
+		$news = News::find($fields);
+		foreach ($news as $item) {
+			echo("<div id='news' class='container'>");
+			echo("<h2>" . $item['title'] . "</h2>");
+			echo($item['text']);
+			echo("</div>");
+			echo("<br><div id='metadata' class='text-center'>");
+			echo("Додано " . $item['date'] . " автором " . "<a href='#'>" . $item['name'] . "</a></center>");
+			echo("</div>");
+		}
+?>
+<img src="<?= $dir."/".$files[rand(0,$count_files)] ?>" class="img-thumbnail" alt="PORSCHE" width="800" height="600">
 <p> </p>
 </div>
