34845f804c1f1d93acfc00da9f8dfad513e526da
alexsoft23 <alex.hera.101@gmail.com>
2015-12-26 21:45:32 +0200

    корекція

--- view/add_news.php
+++ view/add_news.php
@@ -1,6 +1,7 @@
 <div class="container text-center">
 <p> </p>
-<img src="./img/872172-porsche-wallpaper.jpg" class="img-thumbnail" alt="PORSCHE" width="400" height="300">
+<?php require_once ('./control/img.php'); ?>
+<img src="<?= $dir."/".$files[rand(0,$count_files)] ?>" class="img-thumbnail" alt="PORSCHE" width="400" height="300">
 <p> </p>
 </div>
 
@@ -10,18 +11,20 @@
 			<div class="col-sm-2 text-left">
 				<p>Тут ви можете додати свою новину. Новина буде додана після перевірки адміністратором.</p>
 			</div>
-			<div class="col-sm-6 slideanim text-right">
-				<div class="row">
-					<div class="col-sm-12 form-group">
-						<input class="form-control" id="name" name="name" placeholder="Тема новини" type="text" required>
+			<form action="index.php?action=news_add" method="POST">
+				<div class="col-sm-6 slideanim text-right">
+					<div class="row">
+						<div class="col-sm-12 form-group">
+							<input class="form-control" id="name" placeholder="Тема новини" type="text" rows="2" name = "news_title">
+						</div>
+					</div>
+					<textarea class="form-control" id="comments" placeholder="Введіть вашу новину" rows="5" name = "news_text"></textarea><br>
+					<div class="row">
+						<div class="col-sm-12 form-group">
+							<button class="btn btn-default pull-right" type="submit">Надіслати</button>
+						</div>
 					</div>
 				</div>
-				<textarea class="form-control" id="comments" name="comments" placeholder="Введіть вашу новину" rows="5"></textarea><br>
-				<div class="row">
-					<div class="col-sm-12 form-group">
-						<button class="btn btn-default pull-right" type="submit">Надіслати</button>
-					</div>
-				</div>
-			</div>
+			</form>
 		</div>
 	</div>
