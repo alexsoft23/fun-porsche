d9ea04375c6a73989563e38d853de43d2e025506
alexsoft23 <alex.hera.101@gmail.com>
2015-12-26 21:41:40 +0200

    підключення до бд

--- control/bd.php
+++ control/bd.php
@@ -1,3 +1,9 @@
-<?
-	$mysqli = mysqli_connect("localhost", "root", "", "porshe");
-?>
+<?php
+	$link = new mysqli("localhost", "root", "", "porshe");
+	if ($link->connect_errno) {
+			echo "Database error <br>";
+			echo "Connect failed: <br>".$link->connect_error;
+			$correct = false;
+			
+			exit();
+		}
