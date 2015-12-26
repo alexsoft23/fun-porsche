72679d73af596ed1c7f43c2245a556cfff843ac6
alexsoft23 <alex.hera.101@gmail.com>
2015-12-26 21:43:23 +0200

    реєстрація за допомогою моделі

--- control/save_user.php
+++ control/save_user.php
@@ -2,41 +2,58 @@
 <?php
 	$correct = true;
 	if (!isset ($_POST['name']) || strlen($_POST['name']) == 0) {
-		echo "<br><br>Поле нікнейм незаповнене<br><br>";
+		echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Поле нікнейм незаповнене</div>";
 		$correct = false;
 	}
 	if (!isset ($_POST['email']) || strlen($_POST['email']) == 0) {
-		echo "<br><br>Поле електрона пошта незаповнене<br><br>";
+		echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Поле електрона пошта незаповнене</div>";
 		$correct = false;
 	}
 	else if (!preg_match("/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/", $_POST['email'])){
 		$correct = false;
-		echo "<br><br>Некоректна електрона пошта<br><br>";
+		echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Некоректна електрона пошта</div>";
 	}
 	if (!isset ($_POST['password']) || strlen($_POST['password']) < 3) {
-		echo "<br><br>Пароль закороткий<br><br>";
+		echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Пароль закороткий</div>";
 		$correct = false;
 	}
 	else if (!isset ($_POST['password_again']) || ($_POST['password']) != $_POST['password_again']) {
-		echo "<br><br>Паролі неспівпадають<br><br>";
+		echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Паролі неспівпадають</div>";
 		$correct = false;
 	}
 	
 	if ($correct) {
 
-	$mysqli = new mysqli("localhost", "root", "", "porshe");
-	if ($mysqli->connect_errno) {
-			echo "Database error <br>";
-			echo "Connect failed: <br>".$mysqli->connect_error;
-			$correct = false;
-			
-			exit();
-		}
+		//require_once ('./control/bd.php');
 		
-		//require"./control/bd.php";
+			$arr ['name'] = $_POST['name'];
+			$arr['email'] = $_POST['email'];
+			$options = [
+				'cost' => 12,
+				'salt' => 'qwertyuiop[]qwertyuiop[]',
+			];
+			$arr['password'] = password_hash($_POST['password'],  CRYPT_BLOWFISH, $options);
+			$arr['sex'] = ($_POST['gender'] == "man")?true:false;
+			$user->create($arr);
+			if ($link->errno == 0) {
+				if ($correct)
+					echo "<div class='alert alert-success col-sm-10 col-sm-offset-1'><strong>Успіх!</strong> Ви успішно зареєстровані</div>";
+					echo "<img src='./sys_img/success.jpg ?>' class='img-thumbnail' alt='PORSCHE' width='600' height='420'>" ;
+			}
+			else {
+				if ($link->errno == 1062) {
+					echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Юзер з даним іменем або поштою вже зареєстрований виберіть інше і повторіть спробу.</div>";
+					$correct = false;
+				}
+				else{
+					echo 'Database error'.(string)$link->errno;
+					$correct = false;
+					die();
+				}
+			}	
 		
-		else{
-			$name = $_POST['name'];
+		//else{
+			/*($name = $_POST['name'];
 			$email = $_POST['email'];
 			$options = [
 				'cost' => 12,
@@ -47,12 +64,14 @@
 			$query = "INSERT INTO users (email, password, name, sex) VALUES('$email', '$password', '$name', '$sex')";
 			$mysqli->query($query);
 			if ($mysqli->errno == 0) {
-				if ($correct)
-					echo "<br><br>Ви успішно зареєстровані<br><br>";
+				if ($correct){
+						echo "<div class='alert alert-success col-sm-10 col-sm-offset-1'><strong>Успіх!</strong> Ви успішно зареєстровані</div>";
+						echo "<img src='./sys_img/success.jpg ?>' class='img-thumbnail' alt='PORSCHE' width='600' height='420'>" ;
+					}
 			}
 			else {
 				if ($mysqli->errno == 1062) {
-					echo "<br><br>Юзер з даним і менем вже зареєстрований виберіть інше і повторіть спробу.<br><br>";
+					echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Юзер з даним і менем вже зареєстрований виберіть інше і повторіть спробу.</div>";
 					$correct = false;
 				}
 				else{
@@ -60,11 +79,13 @@
 					$correct = false;
 					die();
 				}
-			}	
-		}
+			}*/	
+		//}
 	}
-	else
-		echo "<br><br>Деякі поля некоректні. Повторвть спробу.<br><br>";
+	else{
+		echo "<div class='alert alert-danger col-sm-10 col-sm-offset-1'><strong>Увага!</strong> Деякі поля некоректні. Повторвть спробу.</div>";
+		echo "<img src='./sys_img/Incorrect.jpg ?>' class='img-thumbnail' alt='PORSCHE' width='600' height='420'>" ;
+	}
 	
 ?>
 </center>
